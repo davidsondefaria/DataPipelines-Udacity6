f40722d27d0e86f3fde3d24c35f850e54a491849
davidson <davidson.faria@jovempan.com.br>
2019-11-19 13:04:58 -0300

    First commit: Code without testing

---
+++ plugins/operators/data_quality.py
@@ -0,0 +1,54 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class DataQualityOperator(BaseOperator):
+
+    ui_color = '#89DA59'
+
+    @apply_defaults
+    def __init__(self,
+                 # Define your operators params (with defaults) here
+                 # Example:
+                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 tables=[],
+                 *args, **kwargs):
+
+        super(DataQualityOperator, self).__init__(*args, **kwargs)
+        # Map params here
+        # Example:
+        # self.conn_id = conn_id
+        self.tables = tables
+        self.redshift_conn_id = redshift_conn_id
+
+    def execute(self, context):
+        self.log.info('Testing DataQualityOperator')
+
+        # Reading aws credentials to connect to redshift database
+#         aws_hook = AwsHook(self.aws_credentials_id)
+#         credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info("DataQualityOperator checking data quality")
+        for table in self.tables:
+            self.log.info(f"DataQualityOperator testing {table} table")
+            check = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
+            if len(check) < 1 or len(check[0]) < 1:
+                raise ValueError(f"Data quality check failed. {table} returned no results")
+            if check[0][0] < 1:
+                raise ValueError(f"Data quality check failed. {table} contained 0 rows")
+            logging.info(f"Data quality on table {table} check passed with {check[0][0]} records")
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
---
+++ plugins/operators/load_dimension.py
@@ -0,0 +1,71 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadDimensionOperator(BaseOperator):
+
+    ui_color = '#80BD9E'
+    
+    insert_sql = """
+        TRUNCATE {};
+        INSERT INTO {}
+        {};
+        COMMIT;
+    """
+
+    @apply_defaults
+    def __init__(self,
+                 # Define your operators params (with defaults) here
+                 # Example:
+                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
+                 sql_query="",
+                 *args, **kwargs):
+
+        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+        # Map params here
+        # Example:
+        # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id,
+        self.table=table
+        self.sql_query=sql_query
+
+    def execute(self, context):
+        self.log.info('Testing LoadDimensionOperator')
+        
+        # Reading aws credentials to connect to redshift database
+#         aws_hook = AwsHook(self.aws_credentials_id)
+#         credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+    
+        # Inserting data into fact table
+        self.log.info(f'LoadDimensionOperator inserting data into dimension table: {self.table}')
+        formatted_sql = LoadFactOperator.insert_sql.format(
+            self.table,
+            self.table,
+            self.sql_query
+        )
+        redshift.run(formatted_sql)
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
